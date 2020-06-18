d18010e5d17d1e4cbbff0a32e55cb07aabf4b725
xmdhs <xmdhss@gmail.com>
2020-06-18 22:49:01 +0800

    add Downassets

--- download/downassets.go
+++ download/downassets.go
@@ -8,7 +8,7 @@ import (
 func Downassets(l libraries, typee string) error {
 	for _, v := range l.assetIndex.Objects {
 		for i := 0; i < 3; i++ {
-			err := get(source(`http://resources.download.minecraft.net`+v.Hash[:2]+`/`+v.Hash[2:], typee), `.minecraft/assets/objects/`+v.Hash[:2]+`/`+v.Hash[2:])
+			err := get(source(`http://resources.download.minecraft.net`+v.Hash[:2]+`/`+v.Hash, typee), `.minecraft/assets/objects/`+v.Hash[:2]+`/`+v.Hash)
 			if err != nil {
 				_, ok := err.(*url.Error)
 				if ok {
