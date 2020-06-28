ddeeb650dea2f7cdd8a536ccef5f32cace3960dd
xmdhs <xmdhss@gmail.com>
2020-06-28 23:41:22 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o gml-windows.exe -ldflags "-w -s" main.go
 gzip -f gml-windows.exe
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gml-linux.exe -ldflags "-w -s" main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gml-linux -ldflags "-w -s" main.go
 gzip -f gml-linux.exe
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o gml-darwin.exe -ldflags "-w -s" main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o gml-darwin -ldflags "-w -s" main.go
 gzip -f gml-darwin.exe
