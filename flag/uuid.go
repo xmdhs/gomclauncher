295733024e51475a1dbd631522fa5bf9643d3c7e
xmdhs <xmdhss@gmail.com>
2020-07-02 11:36:35 +0800

    fix

--- flag/uuid.go
+++ flag/uuid.go
@@ -8,9 +8,7 @@ func UUIDgen(t string) string {
 	bb := []byte(t)
 	bb = append(bb, make([]byte, 16)...)
 	bb = bb[0:16]
-	s := make([]byte, hex.EncodedLen(len(bb)))
-	hex.Encode(s, bb)
-	return string(s)
+	return hex.EncodeToString(bb)
 }
 
 func aerr(err error) {
