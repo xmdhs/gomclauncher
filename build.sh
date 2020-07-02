13302416cfcbd4a502f7e4accc3648c4deafc174
xmdhs <xmdhss@gmail.com>
2020-07-02 15:46:31 +0800

    更改脚本

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o gml-windows.exe -trimpath -ldflags "-w -s" main.go
-gzip -f gml-windows.exe
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gml-linux -trimpath -ldflags "-w -s" main.go
-gzip -f gml-linux
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o gml-darwin -trimpath -ldflags "-w -s" main.go
-gzip -f gml-darwin
