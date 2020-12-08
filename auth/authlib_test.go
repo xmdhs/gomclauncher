8a8c3134e2c7993c1db075a893f86e2057d87683
xmdhs <xmdhss@gmail.com>
2020-12-08 21:20:50 +0800

    🐛

--- auth/authlib_test.go
+++ auth/authlib_test.go
@@ -13,7 +13,6 @@ func TestGetauthlibapi(t *testing.T) {
 		wantErr        bool
 	}{
 		{name: "1", args: args{api: "littleskin.cn"}, wantApiaddress: "https://littleskin.cn/api/yggdrasil", wantErr: false},
-		{name: "2", args: args{api: "baidu.com"}, wantApiaddress: "https://baidu.com", wantErr: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
