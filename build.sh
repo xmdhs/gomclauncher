23b8c21aaea1640b06bd4eab18e66534ec027210
xmdhs <xmdhss@gmail.com>
2020-12-15 20:11:58 +0800

    👷

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o gml-windows.exe -trimpath -ldflags "-w -s" main.go
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gml-linux -trimpath -ldflags "-w -s" main.go
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o gml-darwin -trimpath -ldflags "-w -s" main.go
+
+# sha256
+sha256sum gml* > gml-sha256
+cat gml-sha256
+
+# gzip
+gzip --best gml*
