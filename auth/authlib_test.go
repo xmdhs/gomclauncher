c6b021bf9bce74785f44aebce7c31e83250900df
xmdhs <xmdhss@gmail.com>
2021-02-16 15:59:10 +0800

    📝 多了个空格

--- auth/authlib_test.go
+++ auth/authlib_test.go
@@ -13,7 +13,7 @@ func TestGetauthlibapi(t *testing.T) {
 		wantErr        bool
 	}{
 		//{name: "1", args: args{api: "littleskin.cn"}, wantApiaddress: "https://mcskin.littleservice.cn/api/yggdrasil", wantErr: false},
-		{name: "2", args: args{api: "skin.jingqingg.com"}, wantApiaddress: " https://skin.jingqingg.com/api/yggdrasil", wantErr: false},
+		{name: "2", args: args{api: "skin.jingqingg.com"}, wantApiaddress: "https://skin.jingqingg.com/api/yggdrasil", wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
