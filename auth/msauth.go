39cbaf9dac67c6f7a79d3bb80089b3fd732c571e
xmdhs <xmdhss@gmail.com>
2021-10-10 21:27:06 +0800

    fix

--- auth/msauth.go
+++ auth/msauth.go
@@ -113,8 +113,8 @@ func getXSTStoken(Xbltoken string) (string, error) {
 		e := ErrHttpCode{}
 		if errors.As(err, &e) && e.code == 401 {
 			m := map[string]interface{}{}
-			err = json.Unmarshal(b, &m)
-			if err != nil {
+			err1 := json.Unmarshal(b, &m)
+			if err1 != nil {
 				return "", fmt.Errorf("getXSTStoken: %w", err)
 			}
 			code, ok := m["XErr"]
