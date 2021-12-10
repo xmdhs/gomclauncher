270fdfd8d68317e91a453436d53cc984ff7c78d2
xmdhs <xmdhss@gmail.com>
2021-12-10 22:46:55 +0800

    fix test

--- auth/authlib_test.go
+++ auth/authlib_test.go
@@ -12,7 +12,7 @@ func TestGetauthlibapi(t *testing.T) {
 		wantApiaddress string
 		wantErr        bool
 	}{
-		{name: "1", args: args{api: "mcskin.littleservice.cn"}, wantApiaddress: "https://mcskin.littleservice.cn/api/yggdrasil", wantErr: false},
+		{name: "1", args: args{api: "littleskin.cn"}, wantApiaddress: "https://littleskin.cn/api/yggdrasil", wantErr: false},
 		//{name: "2", args: args{api: "skin.jingqingg.com"}, wantApiaddress: "https://skin.jingqingg.com/api/yggdrasil", wantErr: false},
 	}
 	for _, tt := range tests {
