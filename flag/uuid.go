e07bc3e5734391670def7560671d5f05deb98fa1
xmdhs <xmdhss@gmail.com>
2022-02-17 22:34:05 +0800

    修复离线登录无法进入部分版本世界的问题

--- flag/uuid.go
+++ flag/uuid.go
@@ -6,9 +6,13 @@ import (
 )
 
 func UUIDgen(t string) string {
-	data := []byte(t)
-	has := md5.Sum(data)
-	return hex.EncodeToString(has[:])
+	data := []byte("OfflinePlayer:" + t)
+	h := md5.New()
+	h.Write(data)
+	uuid := h.Sum(nil)
+	uuid[6] = (uuid[6] & 0x0f) | uint8((3&0xf)<<4)
+	uuid[8] = (uuid[8] & 0x3f) | 0x80
+	return hex.EncodeToString(uuid)
 }
 
 func aerr(err error) {
