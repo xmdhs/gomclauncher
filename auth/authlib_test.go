b35f3ba2bc7535e1a19ed880fcda1d2a0409336e
xmdhs <xmdhss@gmail.com>
2022-03-09 22:42:48 +0800

    change unit test

--- auth/authlib_test.go
+++ auth/authlib_test.go
@@ -12,8 +12,8 @@ func TestGetauthlibapi(t *testing.T) {
 		wantApiaddress string
 		wantErr        bool
 	}{
-		{name: "1", args: args{api: "littleskin.cn"}, wantApiaddress: "https://littleskin.cn/api/yggdrasil", wantErr: false},
-		//{name: "2", args: args{api: "skin.jingqingg.com"}, wantApiaddress: "https://skin.jingqingg.com/api/yggdrasil", wantErr: false},
+		//{name: "1", args: args{api: "littleskin.cn"}, wantApiaddress: "https://littleskin.cn/api/yggdrasil", wantErr: false},
+		{name: "2", args: args{api: "skin.jingqingg.com"}, wantApiaddress: "https://skin.jingqingg.com/api/yggdrasil", wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
