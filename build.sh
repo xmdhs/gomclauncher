dfbd1d0550cfe047ee362c59afcb2d60667f5a0f
xmdhs <xmdhss@gmail.com>
2022-06-15 07:35:17 +0800

    add darwin/arm64

--- build.sh
+++ build.sh
@@ -3,10 +3,14 @@ LDFLAGS="-X 'main.buildDate=$(date)' -X 'main.gitHash=$(git rev-parse HEAD)' -X 
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o gml-windows.exe -trimpath -ldflags "${LDFLAGS}"
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gml-linux -trimpath -ldflags "${LDFLAGS}"
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o gml-darwin -trimpath -ldflags "${LDFLAGS}"
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o gml-darwin-arm -trimpath -ldflags "${LDFLAGS}"
 
 # sha256
 sha256sum gml* > gml-sha256
 cat gml-sha256
+
+# chmod 
+chmod +x gml-*
 
 # gzip
 gzip --best gml*
